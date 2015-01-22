136a02b709f078d4101ad78c069c3b0f810bb707
Allen Park <allenpark@mit.edu>
2015-01-21 21:18:27 -0500

    cleaned up code and separated db lookups into people.js

--- js/directory.js
+++ js/directory.js
@@ -1,12 +1,12 @@
+var people;
+
 $(document).ready( function(){
+  people = new People();
   // initially hide all rooms
   $('#Rooms').children().children().fadeOut(0);
 });
 
-var input = ""
-function userHasTyped(something) {
-  input = something
-
+function userHasTyped(input) {
   // show everything 
   if (input == "") {
     $('#Facilities, #Dining, #Firestairs, #Elevators, #Lounges').fadeIn(300);
@@ -24,8 +24,9 @@ var bestMatchValue = 0;
 var matchPoints = [];
 
 function getMatches(string) {
+  // TODO: replace with autocomplete
   matchPoints = []
-  for (var i = 0; i < everyone.length; i++) {
+  for (var i = 0; i < people.numPeople(); i++) {
 	matchPoints.push(0)
   }
   results = [];
@@ -39,15 +40,16 @@ function getMatches(string) {
   }
   if (bestMatchValue == 0) return results;
   leniency = 0; /* 0 only shows result which have the most matches.  1 or more shows sets which have "leniency" less matches than the result with the most matches */
-  for (var i = 0; i < everyone.length; i++) {
+  for (var i = 0; i < people.numPeople(); i++) {
     if (matchPoints[i] >= bestMatchValue - leniency) {
-		results.push(everyone[i]);
-     }
+	  results.push(people.getPerson(i));
+    }
   }
   return results;
 }
 
-function search() {
+function search(input) {
+  input = input || $('.search-box').val();
   var results = getMatches(input);
   $('.results').empty();
   $('#Rooms').children().children().fadeOut(300); // clear previous results
@@ -59,8 +61,8 @@ function search() {
 }
 
 function findByRoom(room) {
-  for (var i = 0; i < everyone.length; i++) {
-    if (everyone[i][4] == room) {
+  for (var i = 0; i < people.numPeople(); i++) {
+    if (people.getRoom(i) == room) {
       matchPoints[i] += 1;
       if (matchPoints[i] > bestMatchValue) bestMatchValue = matchPoints[i];
     }
@@ -68,8 +70,8 @@ function findByRoom(room) {
 }
 
 function findByKerb(kerb) {
-  for (var i = 0; i < everyone.length; i++) {
-    if (everyone[i][3] == kerb) {
+  for (var i = 0; i < people.numPeople(); i++) {
+    if (people.getKerb(i) == kerb) {
       matchPoints[i] += 1;
       if (matchPoints[i] > bestMatchValue) bestMatchValue = matchPoints[i];
     }
@@ -77,15 +79,15 @@ function findByKerb(kerb) {
 }
 
 function findByName(name) {
-  name = name.charAt(0).toUpperCase() + name.slice(1)
-  for (var i = 0; i < everyone.length; i++) {
+  name = name.charAt(0).toUpperCase() + name.slice(1);
+  for (var i = 0; i < people.numPeople(); i++) {
     // first name
-    if (everyone[i][1].indexOf(name) != -1) {
+    if (people.getFName(i).indexOf(name) != -1) {
       matchPoints[i] += 1;
       if (matchPoints[i] > bestMatchValue) bestMatchValue = matchPoints[i];
     }
     // last name
-    if (everyone[i][0].indexOf(name) != -1) {
+    if (people.getLName(i).indexOf(name) != -1) {
       matchPoints[i] += 1;
       if (matchPoints[i] > bestMatchValue) bestMatchValue = matchPoints[i];
     }
@@ -93,35 +95,37 @@ function findByName(name) {
 }
 
 function display(result) {
-  var role = ""
+  var role = "";
   if (result[5] != "") {
-    role = " '" + result[5].slice(-2)
+    // TODO: replace with people.getYear(result).slice(-2)
+    role = " '" + result[5].slice(-2);
   }
   else {
+    // TODO: replace with Role field in people
     if (result[4] == "365" || result[4] == "772") {
-      role = " (Housemaster)"
+      role = " (Housemaster)";
     }
     else if (result[4] == "580") {
-      role = " (RLAD)"
+      role = " (RLAD)";
     }
     // hack for apts currently for res. scholars
     else if (result[4] == "436" ||
              result[4] == "480" ||
              result[4] == "528" ||
              result[4] == "1080") {
-      role = " (Residential Scholar)"
+      role = " (Residential Scholar)";
     }
     else {
-      role = " (GRT)"
+      role = " (GRT)";
     }
   }
   $('.results').append(
-    "<div class='result' onclick='input=" + '"' + result[3] + '"' + "; search();'>Rm " + result[4] + " &mdash; <strong>" + result[1] + " " + result[0] + role + "</strong> (" + result[3] + "@mit.edu)" + "</div>" 
+    "<div class='result' onclick='search(\"" + result[3] + "\");'>Rm " + result[4] + " &mdash; <strong>" + result[1] + " " + result[0] + role + "</strong> (" + result[3] + "@mit.edu)" + "</div>" 
   );
   $('#r' + result[4]).fadeIn(300);
 
     // set onclicks for all rooms
-  $('#r' + result[4]).attr("onclick", "input = '" + result[4] + "'; search();");
+  $('#r' + result[4]).attr("onclick", "search(\"" + result[4] + "\");");
   $('#r' + result[4]).attr("style", "cursor: pointer;");
 
   // set titles for tooltips
