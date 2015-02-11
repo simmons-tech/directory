82421f7c062581c7e8baaea69d43a90308f91d55
Allen Park <allenpark@mit.edu>
2015-02-11 09:48:28 -0500

    added new data

--- js/DB-Data-process.py
+++ js/DB-Data-process.py
@@ -1,36 +1,44 @@
 import re
+import csv
 lines = [x.split(',') for x in open('DB-Data.csv').read().split('\n')[1:-1]]
-fout = open('everyone.js', 'w')
-fout.write('var everyone = [\n')
-for entry in lines:
-    entry = [(re.escape(x) if x != '\\N' else '') for x in entry]
-    fout.write('  [\'')
-    fout.write(entry[2] + '\', \'') # lname
-    fout.write(entry[1] + '\', \'') # fname
-    fout.write('' + '\', \'') # title
-    fout.write(entry[0] + '\', \'') # kerb
-    fout.write(entry[3] + '\', \'') # room
-    fout.write(entry[4] + '\', \'') # year
-    if len(entry[7]) > 7:
-        entry[7] = entry[7][8:] # remove 'lounge-'
-    fout.write(entry[7] + '\', \'') # lounge
-    t = ''
-    if entry[5] == 'U':
-        t = 'Undergraduate'
-    elif entry[5] == 'VS':
-        t = 'Visiting Scholar'
-    elif entry[5] == 'HM':
-        t = 'Housemaster'
-    elif entry[5] == 'GRT':
-        t = 'GRT'
-    elif entry[5] == 'RLA':
-        t = 'RLAD'
-    elif entry[5] == 'AHM':
-        t = 'Housemaster (Assistant)'
-    elif entry[5] == 'OTHER':
-        t = 'Other'
-    fout.write(t + '\', \'') # type
-    fout.write(entry[6] + '\'],\n') # email
-fout.write(']\n')
-fout.close()
-
+with open('DB-Data.csv', 'rb') as csvfile:
+    lines = csv.reader(csvfile, delimiter=',', quotechar='"')
+    fout = open('everyone.js', 'w')
+    fout.write('var everyone = [\n')
+    lines.next() # skip first line
+    for entry in lines:
+        entry = [(re.escape(x) if x != '\\N' else '') for x in entry]
+        fout.write('  [\'')
+        fout.write(entry[2] + '\', \'') # lname
+        fout.write(entry[1] + '\', \'') # fname
+        fout.write('' + '\', \'') # title
+        fout.write(entry[0] + '\', \'') # kerb
+        fout.write(entry[3] + '\', \'') # room
+        fout.write(entry[5] + '\', \'') # year
+        lounge = entry[17]
+        if len(lounge) > 7:
+            lounge = lounge[8:] # remove 'lounge-'
+        fout.write(lounge + '\', \'') # lounge
+        t = ''
+        tr = entry[15]
+        if tr == 'U':
+            t = 'Undergraduate'
+        elif tr == 'VS':
+            t = 'Visiting Scholar'
+        elif tr == 'HM':
+            t = 'Housemaster'
+        elif tr == 'GRT':
+            t = 'GRT'
+        elif tr == 'RLA':
+            t = 'RLAD'
+        elif tr == 'AHM':
+            t = 'Housemaster (Assistant)'
+        elif tr == 'OTHER':
+            t = 'Other'
+        fout.write(t + '\', \'') # type
+        fout.write(entry[16] + '\', \'') # email
+        fout.write(entry[18] + '\'') # title
+        fout.write('],\n')
+    fout.write(']\n')
+    fout.close()
+print 'Done'
