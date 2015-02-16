99332321bf5de9540b5214d0cb109cb0e5fce7b1
Allen Park <allenpark@mit.edu>
2015-02-16 00:20:02 -0500

    I fucked up again

--- directory/settings.py
+++ directory/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '(echxevhb8hnhy#n884nfbivj^a5o%tf(1wr5jf@b3c2udyewg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 TEMPLATE_DEBUG = True
 
